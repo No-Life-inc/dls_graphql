40d43320258e8220fab382cb2b586e2260a1af71
Morten Bendeke <mbendeke@hotmail.com>
2024-03-21 15:24:45 +0100

    added params in userSchema

--- mongoose/schemas/userSchema.ts
+++ mongoose/schemas/userSchema.ts
@@ -4,11 +4,13 @@ import userInfoSchema from './userInfoSchema';
 /**
  * Define the User Schema
  * @param {String} userId - The ID of the user
+ * @param {Date} createdAt - The date the user was created
  * @param {Object} userInfo - The user's information
  */
 
 const userSchema = new mongoose.Schema({
     _id: String,
+    createdAt: Date,
     userInfo: userInfoSchema
 
 });
