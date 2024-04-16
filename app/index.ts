8772b6b068adae4ebea5c322e99a42b542f50f3e
Yelong Hartl-He <Yelong.he@hotmail.com>
2024-04-16 14:29:48 +0200

    added jwt-verification

--- app/index.ts
+++ app/index.ts
@@ -1,11 +1,12 @@
 import { ApolloServer } from "apollo-server-express";
 import Schema from "../graphql/Schema";
 import Query from "../graphql/resolvers/Query";
-import express from "express";
+import express, {Request, Response, NextFunction}from "express";
 import { ApolloServerPluginDrainHttpServer } from "apollo-server-core";
 import http from "http";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
+import jwt from 'jsonwebtoken'
 
 dotenv.config();
 
@@ -14,12 +15,39 @@ const port = process.env.PORT || 3000;
 const mongoUser = process.env.MONGOUSER;
 const mongoPw = process.env.MONGOPW;
 const mongoUrl = process.env.MONGOURL;
+const jwtsecret = process.env.JWTSECRET || "";
 
 const mongoUri = `mongodb://${mongoUser}:${mongoPw}${mongoUrl}`;
+
+declare global {
+  namespace Express {
+    interface Request {
+      user?: any;
+    }
+  }
+}
 
 async function startApolloServer(schema: any, resolvers: any) {
   const app = express();
   const httpServer = http.createServer(app);
+
+  const verifyToken = (req: Request,res: Response,next: NextFunction)=>{
+    const token = req.headers.authorization;
+
+    if (!token){
+      return res.status(401).json({message: 'Missing JWT-token'})
+    }
+
+    jwt.verify(token,jwtsecret, (err: any,decoded:any)=>{
+      if (err){
+        return res.status(401).json({message: 'Invalid JWT-token'});
+      }
+      req.user = decoded;
+      next();
+    });
+  };
+
+  app.use('/graphql', verifyToken)
 
   // Connect to MongoDB
   await mongoose.connect(mongoUri, {});
